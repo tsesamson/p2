7779f52b5d2fab65709f14b742d4bb2cd3eab280
Samson Tse <tse.samson@gmail.com>
2015-09-18 20:22:56 -0400

    Initial upload with README.md and index.php file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Samson Tse's Project 2
+
+## Live URL
+<http://p2.samsontse.com>
+
+## Description
+The goal of this project is to create a usable xkcd password generator.  A xkcd password is ... TODO.
+
+## Demo/Screencast
+Not required for this project.
+
+## Details for teaching team
+No login required for this password generator.
+
+## Outside Code
+* jQuery: https://jquery.com/
+* Bootstrap: http://getbootstrap.com
+* Bootstrap Theme: http://bootswatch.com/flatly/
+
+## Wordlist Source
+* corncob_lowercase.txt: http://mieliestronk.com/
---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Samson Tse P2 - XKCD Password Genertor</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+	
+	<style>
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			min-width:600px;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:50%;
+		}
+		.hero-unit {	
+			padding:20px;
+			margin-bottom:30px;
+			font-size:18px;
+			font-weight:200;
+			line-height:30px;
+			color:inherit;
+			background-color:#eee;
+			border-radius:6px;
+		}
+	</style>
+
+</head>
+<body>
+
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-header">
+          <h1>XKCD Password Generator</h1>
+        </div>
+      </div>
+    </div>
+		
+    <div class="hero-unit">
+	<h1>Password Result</h1>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="panel panel-default" style="margin-top:25px;">
+          <div class="panel-heading">
+            <h2 class="panel-title">Blah blah</h2>
+          </div>
+          <div class="panel-body">
+	  blah blah
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+	</div>
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+</body>
+</html>
